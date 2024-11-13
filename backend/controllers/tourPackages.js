e3654002f03207306a43a72d5af37fa3b3c09023
AhmadAlHammad <Alhammmadahmad@gmail.com>
2024-11-13 19:43:13 +0300

    Update tour packages

--- backend/controllers/tourPackages.js
+++ backend/controllers/tourPackages.js
@@ -1,12 +1,9 @@
 const db = require("../models/db");
 
-//create tour packages 
-
-
-
-
+//create tour packages
 
 const createTourPackages = async (req, res) => {
+  // const user_id = req.token.user_id;
   const {
     name,
     destination,
@@ -28,7 +25,7 @@ const createTourPackages = async (req, res) => {
     hotel_name,
     description,
     price,
-    image_url,is_deleted) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,9,0) RETURNING *`;
+    image_url,is_deleted) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,0) RETURNING *`;
 
   const values = [
     name,
@@ -77,7 +74,63 @@ const getAllTourPakages = async (req, res) => {
 };
 
 //add put to Admin
-//This function delete tour By id 
+const updateTourPackage = async (req, res) => {
+  const tour_packages_id = req.params.tour_packages_id;
+
+  const { 
+    name, 
+    destination, 
+    duration_days, 
+    start_date, 
+    end_date, 
+    hotel_name, 
+    description, 
+    price, 
+    image_url 
+  } = req.body;  
+  const query = `
+    UPDATE tour_packages 
+    SET 
+      name = $1,
+      destination = $2,
+      duration_days = $3,
+      start_date = $4,
+      end_date = $5,
+      hotel_name = $6,
+      description = $7,
+      price = $8,
+      image_url = $9
+    WHERE tour_packages_id = $10
+    RETURNING *;
+  `;
+
+  const values = [
+    name,
+    destination,
+    duration_days,
+    start_date,
+    end_date,
+    hotel_name,
+    description,
+    price,
+    image_url,
+    tour_packages_id,
+  ];
+
+  try {
+    const result = await db.query(query, values);
+res.status(200).json({success : true , message : "Update successfully"})
+  
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      err: error.message,
+    });
+  }
+};
+//This function delete tour By id
 
 const deletedById = async (req, res) => {
   const tour_packages_id = req.params.tour_packages_id;
@@ -96,4 +149,9 @@ const deletedById = async (req, res) => {
   }
 };
 
-module.exports = { createTourPackages, getAllTourPakages, deletedById };
+module.exports = {
+  createTourPackages,
+  getAllTourPakages,
+  updateTourPackage,
+  deletedById,
+};
--- backend/routes/tourPagesRoute.js
+++ backend/routes/tourPagesRoute.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { createTourPackages, getAllTourPakages, deletedById } = require("../controllers/tourPackages");
-
+const { createTourPackages, getAllTourPakages,updateTourPackage,deletedById } = require("../controllers/tourPackages");
+const authentication  = require("../middlewares/authentication");
 const tour_packagesRouter = express.Router();
 
 
 
 tour_packagesRouter.post("/createTour", createTourPackages);
+tour_packagesRouter.put("/update/:tour_packages_id", updateTourPackage);
+
 tour_packagesRouter.get("/all", getAllTourPakages);
 tour_packagesRouter.delete("/tour/:tour_packages_id", deletedById);
 
