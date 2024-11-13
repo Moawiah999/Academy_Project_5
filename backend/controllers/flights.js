c3045402a1c071bfed010e2150eea75fe99ebdb1
Moawiah999 <muawiyah454@gmail.com>
2024-11-13 22:07:00 +0300

    cancel Flight

--- backend/controllers/flights.js
+++ backend/controllers/flights.js
@@ -66,7 +66,39 @@ const bookFlight = (req, res) => {
       });
     });
 };
-const findAtrip= (req,res)=>{
+const findAtrip = (req, res) => {};
+const cancelFlight = (req, res) => {
+  // user_id from token
+  const { user_id, flights_id } = req.body;
 
-}
-module.exports = { createFlights, bookFlight,findAtrip };
+  const query = `
+    DELETE FROM userFlight
+    WHERE user_id = $1 AND flights_id = $2
+    RETURNING *;
+  `;
+  const values = [user_id, flights_id];
+
+  db.query(query, values)
+    .then((result) => {
+      if (result.rowCount === 0) {
+        res.status(404).json({
+          success: false,
+          message: "No booking found to cancel",
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: "Flight booking canceled successfully",
+          result: result.rows[0],
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        err: err,
+      });
+    });
+};
+module.exports = { createFlights, bookFlight, findAtrip, cancelFlight };
