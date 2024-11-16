f1e453beb915d7fc390472bd6b5df6adb7ff0349
Mosouri <mohommad.souri@gmail.com>
2024-11-16 17:17:40 +0300

    NavBar 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,13 @@
+import React from 'react'
+import NavbarPage from './Component/NavBar/navbar'
+import "bootstrap/dist/css/bootstrap.min.css";
 
+
+const App = () => {
+  return (<>
+    <NavbarPage/>
+  </>
+  )
 }
 
-export default App;
+export default App
