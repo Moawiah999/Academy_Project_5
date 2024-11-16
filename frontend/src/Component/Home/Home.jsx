0ed7ca7c3b2f7f3d52210d56451632079700ab17
AhmadAlHammad <Alhammmadahmad@gmail.com>
2024-11-16 21:56:56 +0300

    update slider and create about us and update navbar

--- frontend/src/Component/Home/Home.jsx
+++ frontend/src/Component/Home/Home.jsx
@@ -1,35 +1,11 @@
 import React from 'react'
-import Carousel from 'react-bootstrap/Carousel';
-
+import ImageSlider from '../imageslider/ImageSlider';
+import AboutUs from '../AboutUs/AboutUs';
 const Home = () => {
   return (
   <>
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        {/* <ExampleCarouselImage text="First slide" /> */}
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQtY0pssXxkH_76eqltSvsCmUcFZECasnffA&s' />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        {/* <ExampleCarouselImage src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQtY0pssXxkH_76eqltSvsCmUcFZECasnffA&s' text="Second slide" /> */}
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="Third slide" /> */}
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+  <ImageSlider/>
+  <AboutUs/>
   </>
   )
 }
