629c93a3f9541250938255203d56992177babc82
AhmadAlHammad <Alhammmadahmad@gmail.com>
2024-11-17 21:27:11 +0300

    styling bootstrab

--- frontend/src/Component/AboutUs/AboutUs.jsx
+++ frontend/src/Component/AboutUs/AboutUs.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const AboutUs = () => {
   return (
-
-    <div className='containerAboutUs'>
-        <h1 className='header'>ABOUT US</h1>
-        <p className='paragraph'>Quick Reserve Pro, established in 2023, specializes in seamless reservation solutions for hotels, flights, and travel packages. Our mission is to provide a user-friendly platform that simplifies bookings, ensuring efficiency, convenience, and satisfaction for both businesses and customers. By offering a comprehensive range of travel services in one place, we aim to revolutionize the way people plan and book their trips, making the process faster, smoother, and more reliable for all involved.
-</p>
-<div className='imageAboutUs'>
-  
-    <img src='https://themewagon.github.io/keto/images/about.png'/>
-</div>
+    <div className="containerAboutUs">
+      <h1 className="header">ABOUT US</h1>
+      <p className="paragraph">
+        Quick Reserve Pro, established in 2023, specializes in seamless
+        reservation solutions for hotels, flights, and travel packages. Our
+        mission is to provide a user-friendly platform that simplifies bookings,
+        ensuring efficiency, convenience, and satisfaction for both businesses
+        and customers. By offering a comprehensive range of travel services in
+        one place, we aim to revolutionize the way people plan and book their
+        trips, making the process faster, smoother, and more reliable for all
+        involved.
+      </p>
+    <>
+    <img src="https://images.pexels.com/photos/8828448/pexels-photo-8828448.jpeg?auto=compress&cs=tinysrgb&w=600" class="rounded float-left" alt="..."></img>
+    </>
     </div>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
--- frontend/src/Component/Home/Home.jsx
+++ frontend/src/Component/Home/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ImageSlider from '../imageslider/ImageSlider';
 import AboutUs from '../AboutUs/AboutUs';
+import ContactUs from '../Contact Us/ContactUs';
 const Home = () => {
   return (
-  <>
+    <>
+  <div>
+    
   <ImageSlider/>
   <AboutUs/>
+  {/* <ContactUs/> */}
+  </div>
+  <ContactUs/> 
   </>
+
   )
 }
 
