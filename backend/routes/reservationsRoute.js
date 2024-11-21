b5d8272aaf93d56fb98a2b898a8856abc92cfb5d
Mosouri <mohommad.souri@gmail.com>
2024-11-21 22:35:48 +0300

    reservation

--- backend/routes/reservationsRoute.js
+++ backend/routes/reservationsRoute.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
 const reservationsRoute = express.Router();
+const { authentication } = require("../middlewares/authentication");
+const {
+  getReservationsById,
+  reservation,
+  updateReservation,
+} = require("../controllers/reservations");
 
-
-const {getReservationsById} = require('../controllers/reservations');
-
-reservationsRoute.get('/allReservations/:user_id',getReservationsById);
+reservationsRoute.get("/allReservations/:user_id", getReservationsById);
+reservationsRoute.post("/", authentication, reservation);
+reservationsRoute.put("/", authentication, updateReservation);
 
 module.exports = reservationsRoute;
