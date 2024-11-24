bdd6f489c1595353308dc69717faa854340c6938
AhmadAlHammad <Alhammmadahmad@gmail.com>
2024-11-24 21:47:42 +0300

    update

--- frontend/src/Component/Packages/Packages.jsx
+++ frontend/src/Component/Packages/Packages.jsx
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Button, Modal, Spinner, Row, Col, Form } from "react-bootstrap";
+import "./Packages.css";
 import { ToastContainer, toast } from "react-toastify";
 import { RiVisaFill } from "react-icons/ri";
 import { FaCreditCard, FaCalendarAlt, FaLock } from "react-icons/fa";
 import "react-toastify/dist/ReactToastify.css";
-import "./Packages.css";
+import Carousel from "react-bootstrap/Carousel";
 
 const Packages = () => {
   const [tourPackages, setTourPackages] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [modalState, setModalState] = useState({
-    showDetails: false,
-    showPayment: false,
-    selectedPackage: null,
-  });
+  const [showPayment, setShowPayment] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPackage, setSelectedPackage] = useState(null);
 
   useEffect(() => {
     axios
@@ -30,20 +29,26 @@ const Packages = () => {
       });
   }, []);
 
-  const handleShowDetails = (packageData) => {
-    setModalState({ ...modalState, showDetails: true, selectedPackage: packageData });
+  const handleCardClick = (packageData) => {
+    setSelectedPackage(packageData);
+    setShowModal(true);
   };
 
-  const handleCloseDetails = () => {
-    setModalState({ ...modalState, showDetails: false, selectedPackage: null });
+  const handleCloseModal = () => {
+    setShowModal(false);
   };
 
-  const handleShowPayment = () => {
-    setModalState({ ...modalState, showPayment: true });
+  const handleCardModal = () => {
+    setShowPayment(true);
   };
 
   const handleClosePayment = () => {
-    setModalState({ ...modalState, showPayment: false });
+    setShowPayment(false);
+  };
+
+  const shortDescription = (description) => {
+    const words = description.split(" ");
+    return words.slice(0, 4).join(" ") + " ...";
   };
 
   const handlePayment = (event) => {
@@ -54,98 +59,131 @@ const Packages = () => {
 
   if (loading) {
     return (
-      <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
         <Spinner animation="border" variant="primary" />
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
-        <p className="text-danger">{error}</p>
+        
       </div>
     );
   }
 
   return (
-    <div className="packages-container" style={{ fontFamily: "Roboto, sans-serif" }}>
-      <Row xs={1} sm={2} md={3} lg={4} className="g-4 justify-content-center" style={{ gap: "70px" ,marginBottom:"100px" , marginTop:"30px"}}>
-        {tourPackages.map((item, index) => (
-          <Col key={index} className="d-flex justify-content-center">
-            <div
-              className="package-item"
-              style={{
-                cursor: "pointer",
-                maxWidth: "350px",
-                margin: "0 auto",
-                background: "#fff",
-                border: "1px solid #ddd",
-                borderRadius: "8px",
-                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-              }}
-            >
-              <img
-                src={item.image_url}
-                alt={`Image of ${item.destination}`}
+    <div
+      className="packages-container"
+      style={{ fontFamily: "Roboto, sans-serif" , marginBottom : "70px",marginTop:"30px"}}
+    >
+      <div
+        className="d-flex justify-content-center align-items-center flex-column"
+        style={{ minHeight: "80vh" }}
+      >
+        <Row
+          xs={1}
+          sm={2}
+          md={3}
+          lg={4}
+          className="g-4 justify-content-center"
+          style={{ gap: "70px" }}
+        >
+          {tourPackages.map((item, index) => (
+            <Col key={index} className="d-flex justify-content-center">
+              <div
+                className="package-item"
                 style={{
-                  width: "100%",
-                  height: "200px",
-                  objectFit: "cover",
-                  borderTopLeftRadius: "8px",
-                  borderTopRightRadius: "8px",
+                  cursor: "pointer",
+                  maxWidth: "350px",
+                  margin: "0 auto",
+                  marginBottom: "-30px",
+                  background: "#fff",
+                  border: "1px solid #ddd",
+                  borderRadius: "8px",
+                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
                 }}
-                onClick={() => handleShowDetails(item)}
-              />
-              <div style={{ padding: "16px", background: "#f8f9fa" }}>
-                <h5 className="text-success">{item.destination}</h5>
-                <p className="text-muted">
-                  <strong>Duration:</strong> {item.duration_days} days
-                  <br />
-                  <strong>Price:</strong> <span className="text-success">${item.price}</span>
-                </p>
-                <Button variant="danger" onClick={() => handleShowDetails(item)}>
-                  View Details
-                </Button>
+              >
+                <img
+                  src={item.image_url}
+                  onClick={() => handleCardClick(item)}
+                  alt={item.destination}
+                  style={{
+                    width: "100%",
+                    height: "200px",
+                    objectFit: "cover",
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                  }}
+                />
+                <div style={{ padding: "16px", background: "#f8f9fa" }}>
+                  <h5 className="text-success">{item.destination}</h5>
+                  <p className="text-muted">
+                    <strong>Duration: </strong>
+                    {item.duration_days} days
+                    <br />
+                    <strong>Price: </strong>
+                    <span className="text-success">${item.price}</span>
+                    <p>
+                      <strong>Description:</strong>{" "}
+                      {shortDescription(item.description)}
+                    </p>
+                    <Button variant="danger" onClick={handleCardModal}>
+                      BookNow
+                    </Button>
+                  </p>
+                </div>
               </div>
-            </div>
-          </Col>
-        ))}
-      </Row>
+            </Col>
+          ))}
+        </Row>
+      </div>
 
-      <Modal show={modalState.showDetails} onHide={handleCloseDetails}>
+      <Modal show={showModal} onHide={handleCloseModal} size="xl">
         <Modal.Header closeButton>
-          <Modal.Title>{modalState.selectedPackage?.destination}</Modal.Title>
+          <Modal.Title>{selectedPackage?.destination}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {modalState.selectedPackage && (
+          {selectedPackage && (
             <>
-              <p>
-                <strong>Hotel:</strong> {modalState.selectedPackage.hotel_name}
-              </p>
-              <p>
-                <strong>Duration:</strong> {modalState.selectedPackage.duration_days} days
-              </p>
-              <p>
-                <strong>Price:</strong> ${modalState.selectedPackage.price}
-              </p>
-              <p>
-                <strong>Description:</strong> {modalState.selectedPackage.description}
-              </p>
+              <div className="row">
+                <div className="col-md-4">
+                  <img
+                    className="d-block w-100"
+                    src={selectedPackage?.image_url}
+                    alt="Package image"
+                  />
+                </div>
+
+                <div className="col-md-8">
+                  <p>
+                    <strong>Hotel :</strong> {selectedPackage?.hotel_name}
+                  </p>
+                  <p>
+                    <strong>Duration :</strong> {selectedPackage?.duration_days}{" "}
+                    days
+                  </p>
+                  <p>
+                    <strong>Price :</strong> {selectedPackage?.price}
+                  </p>
+                  <p>
+                    <strong>Description :</strong>{" "}
+                    {selectedPackage?.description}
+                  </p>
+                </div>
+              </div>
             </>
           )}
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={handleShowPayment}>
-            Proceed to Payment
+
+        <Modal.Footer className="d-flex justify-content-center">
+          <Button variant="danger" onClick={handleCardModal}>
+            BookNow
           </Button>
-          <Button variant="secondary" onClick={handleCloseDetails}>
+          <Button variant="secondary" onClick={handleCloseModal}>
             Close
           </Button>
         </Modal.Footer>
       </Modal>
 
-      <Modal show={modalState.showPayment} onHide={handleClosePayment}>
+      <Modal show={showPayment} onHide={handleClosePayment}>
         <Modal.Header closeButton>
           <Modal.Title>Payment Information</Modal.Title>
         </Modal.Header>
@@ -155,31 +193,51 @@ const Packages = () => {
               <Form.Label>Card Number</Form.Label>
               <div className="d-flex align-items-center">
                 <FaCreditCard className="me-2" />
-                <Form.Control type="text" name="cardNumber" placeholder="Enter card number" required />
+                <Form.Control
+                  type="text"
+                  name="cardNumber"
+                  placeholder="Enter card number"
+                  required
+                />
               </div>
             </Form.Group>
             <Form.Group>
               <Form.Label>Expiration Date</Form.Label>
               <div className="d-flex align-items-center">
                 <FaCalendarAlt className="me-2" />
-                <Form.Control type="text" name="expiryDate" placeholder="MM/YY" required />
+                <Form.Control
+                  type="text"
+                  name="experinceDate"
+                  placeholder="MM/YY"
+                  required
+                />
               </div>
             </Form.Group>
             <Form.Group>
               <Form.Label>CVV</Form.Label>
               <div className="d-flex align-items-center">
                 <FaLock className="me-2" />
-                <Form.Control type="text" name="cvv" placeholder="Enter CVV" required />
+                <Form.Control
+                  type="text"
+                  name="cvv"
+                  placeholder="Enter CVV"
+                  required
+                />
               </div>
             </Form.Group>
             <Form.Group>
               <Form.Label>Cardholder Name</Form.Label>
               <div className="d-flex align-items-center">
                 <RiVisaFill className="me-2" />
-                <Form.Control type="text" name="cardHolderName" placeholder="Enter cardholder name" required />
+                <Form.Control
+                  type="text"
+                  name="cardHolderName"
+                  placeholder="Enter cardholder name"
+                  required
+                />
               </div>
             </Form.Group>
-            <Modal.Footer>
+            <Modal.Footer className="d-flex justify-content-center">
               <Button variant="danger" type="submit">
                 Confirm Payment
               </Button>
