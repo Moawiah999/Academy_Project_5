075ec295d2808bea75d942d758375a98016556e9
Moawiah999 <muawiyah454@gmail.com>
2024-11-28 21:45:50 +0300

    flights reservation

--- backend/routes/reservationsRoute.js
+++ backend/routes/reservationsRoute.js
@@ -5,11 +5,15 @@ const { authentication } = require("../middlewares/authentication");
 const {
   getReservationsById,
   updateReservation,
-  createReservation,
+  createFlightsReservation,
 } = require("../controllers/reservations");
 
 reservationsRoute.get("/allReservations/:user_id", getReservationsById);
-reservationsRoute.post("/", authentication, createReservation);
+reservationsRoute.post(
+  "/reservationFlight",
+  authentication,
+  createFlightsReservation
+);
 reservationsRoute.put("/", authentication, updateReservation);
 
 module.exports = reservationsRoute;
