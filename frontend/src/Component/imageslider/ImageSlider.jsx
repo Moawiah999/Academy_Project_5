e0e6e5fd989b6181a602126d24f082cf9549ad9e
Moawiah999 <muawiyah454@gmail.com>
2024-11-29 21:01:15 +0300

    update

--- frontend/src/Component/imageslider/ImageSlider.jsx
+++ frontend/src/Component/imageslider/ImageSlider.jsx
@@ -7,8 +7,7 @@ const ImageSlider = () => {
     <div className="slider">
       <Carousel className="slider p-2" style={{"borderRadius":"40px"}}>
         <Carousel.Item interval={5000}>
-          <img
-            src="/images\home_image_1.jpeg"
+       
 
           <img style={{ "border-radius": "25px" }}
             src="https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg"
@@ -18,8 +17,7 @@ const ImageSlider = () => {
         </Carousel.Item>
 
         <Carousel.Item interval={5000}>
-          <img
-            src="/images\home_image_2.jpeg"
+     
           <img style={{ "border-radius": "25px" }}
             src="https://images.pexels.com/photos/1051075/pexels-photo-1051075.jpeg"
             alt="Second slide"
@@ -28,8 +26,8 @@ const ImageSlider = () => {
         </Carousel.Item>
 
         <Carousel.Item interval={5000}>
-          <img
-            src="/images\home_image_3.jpg
+          
+            
           <img style={{ "border-radius": "25px" }}
             src="https://themewagon.github.io/keto/images/banner3.jpg"
             alt="Third slide"
